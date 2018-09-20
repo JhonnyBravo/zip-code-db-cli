56a600b2687c63f5e1e5e4552815dc915c6de487
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-09-20 22:15:36 +0900

    modified: src/test/java/zip_code_db_cli/GetPathListTest.java
    GetPathList の仕様変更に伴うコード修正。

--- src/test/java/zip_code_db_cli/GetPathListTest.java
+++ src/test/java/zip_code_db_cli/GetPathListTest.java
@@ -1,33 +1,26 @@
-/**
- *
- */
 package zip_code_db_cli;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
 import org.junit.Test;
 
-/**
- * @author sanfr
- *
- */
 public class GetPathListTest {
 
     /**
-    * {@link zip_code_db_cli.GetPathList#runCommand()},
-    * {@link zip_code_db_cli.GetPathList#getPathList()},
-    * {@link zip_code_db_cli.GetPathList#getCode()} のためのテスト・メソッド。
-    */
+     * {@link zip_code_db_cli.GetPathList#runCommand()},
+     * {@link zip_code_db_cli.GetPathList#getPathList()},
+     * {@link zip_code_db_cli.GetPathList#getCode()} のためのテスト・メソッド。
+     */
     @Test
     public void test1() {
         GetPathList gpl = new GetPathList("src/test/resources", "csv");
         gpl.runCommand();
         assertEquals(2, gpl.getCode());
 
-        List<String> pathList = gpl.getPathList();
-        assertEquals(1,pathList.size());
+        List<String> pathList = gpl.runCommand();
+        assertEquals(1, pathList.size());
 
         for (String path : pathList) {
             System.out.println(path);
@@ -37,20 +30,18 @@ public class GetPathListTest {
     @Test
     public void test2() {
         GetPathList gpl = new GetPathList("src/test/resources/NotExist", "csv");
-        gpl.runCommand();
-        assertEquals(1, gpl.getCode());
+        List<String> pathList = gpl.runCommand();
 
-        List<String> pathList=gpl.getPathList();
-        assertEquals(null,pathList);
+        assertEquals(1, gpl.getCode());
+        assertEquals(null, pathList);
     }
 
     @Test
     public void test3() {
         GetPathList gpl = new GetPathList("src/test/resources", "txt");
-        gpl.runCommand();
-        assertEquals(0, gpl.getCode());
+        List<String> pathList = gpl.runCommand();
 
-        List<String> pathList=gpl.getPathList();
-        assertEquals(0,pathList.size());
+        assertEquals(0, gpl.getCode());
+        assertEquals(0, pathList.size());
     }
 }
