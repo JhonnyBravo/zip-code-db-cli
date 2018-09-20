24f02e2c2802734c41f48552008a7c013676e86c
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-09-20 22:09:43 +0900

    modified: src/main/java/zip_code_db_cli/GetPathList.java
    TZipCodeState の利用を廃止。
    status_resource.StatusController を継承する方式に変更。

--- src/main/java/zip_code_db_cli/GetPathList.java
+++ src/main/java/zip_code_db_cli/GetPathList.java
@@ -6,18 +6,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import status_resource.StatusController;
+
 /**
  * 指定したディレクトリ配下に存在するファイルの一覧を取得する。
  */
-public class GetPathList implements TZipCodeState {
-    private int code;
-    private String message;
+public class GetPathList extends StatusController {
     private String path;
     private String extension;
-    private List<String> pathList=null;
 
     /**
-     * @param path 検索対象とするディレクトリのパスを指定する。
+     * @param path      検索対象とするディレクトリのパスを指定する。
      * @param extension 検索対象とするファイルの拡張子を指定する。
      */
     public GetPathList(String path, String extension) {
@@ -26,20 +25,13 @@ public class GetPathList implements TZipCodeState {
     }
 
     /**
-     * 終了コードとメッセージを初期化する。
+     * 指定したディレクトリの配下から条件に合致するファイルのパスを取得し、リストへ格納して返す。
+     * 
+     * @return List&lt;String&gt; ファイルパスを格納したリストを返す。
      */
-    @Override
-    public void initStatus() {
-        this.code = 0;
-        this.message = null;
-    }
-
-    /**
-     * 指定したディレクトリの配下から条件に合致するファイルのパスを取得し、リストへ格納する。
-     */
-    @Override
-    public void runCommand() {
+    public List<String> runCommand() {
         this.initStatus();
+        List<String> pathList = null;
 
         FilenameFilter filter = new FilenameFilter() {
 
@@ -56,49 +48,29 @@ public class GetPathList implements TZipCodeState {
         File directory = new File(this.path);
 
         if (directory.isDirectory() == false) {
-            this.message = this.path + " が見つかりません。";
+            this.setMessage(this.path + " が見つかりません。");
             this.errorTerminate();
-            return;
+            return pathList;
         }
 
-        this.pathList = new ArrayList<String>();
+        pathList = new ArrayList<String>();
         File[] fileList = directory.listFiles(filter);
 
         for (int i = 0; i < fileList.length; ++i) {
             try {
-                this.pathList.add(fileList[i].getCanonicalPath());
+                pathList.add(fileList[i].getCanonicalPath());
             } catch (IOException e) {
-                this.message = "エラーが発生しました。 " + e.toString();
+                this.setMessage("エラーが発生しました。 " + e.toString());
                 this.errorTerminate();
+                return pathList;
             }
         }
 
-        if (this.pathList.size() > 0) {
-            this.code = 2;
-            this.message = null;
+        if (pathList.size() > 0) {
+            this.setCode(2);
+            this.setMessage(null);
         }
-    }
 
-    /**
-     * エラーメッセージを出力する。
-     */
-    @Override
-    public void errorTerminate() {
-        this.code = 1;
-        System.err.println(this.message);
-    }
-
-    /**
-     * @return pathList ディレクトリ配下に存在するファイルのパス一覧を返す。
-     */
-    public List<String> getPathList() {
         return pathList;
-    }
-
-    /**
-     * @return code 終了ステータスを返す。
-     */
-    public int getCode() {
-        return code;
     }
 }
