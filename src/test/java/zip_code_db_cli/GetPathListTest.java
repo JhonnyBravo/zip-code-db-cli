4687f9fa05e3f2d809455fba07408cbfcd760ffb
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-11-07 21:18:38 +0900

    modified: src/test/java/zip_code_db_cli/GetPathListTest.java
    GetPathList クラスの仕様変更に伴うコード修正。

--- src/test/java/zip_code_db_cli/GetPathListTest.java
+++ src/test/java/zip_code_db_cli/GetPathListTest.java
@@ -7,19 +7,17 @@ import java.util.List;
 import org.junit.Test;
 
 public class GetPathListTest {
+    private GetPathList gpl = new GetPathList();
 
     /**
      * {@link zip_code_db_cli.GetPathList#runCommand()},
-     * {@link zip_code_db_cli.GetPathList#getPathList()},
      * {@link zip_code_db_cli.GetPathList#getCode()} のためのテスト・メソッド。
      */
     @Test
     public void test1() {
-        GetPathList gpl = new GetPathList("src/test/resources", "csv");
-        gpl.runCommand();
-        assertEquals(2, gpl.getCode());
-
+        gpl.init("src/test/resources", "csv");
         List<String> pathList = gpl.runCommand();
+        assertEquals(2, gpl.getCode());
         assertEquals(1, pathList.size());
 
         for (String path : pathList) {
@@ -29,7 +27,7 @@ public class GetPathListTest {
 
     @Test
     public void test2() {
-        GetPathList gpl = new GetPathList("src/test/resources/NotExist", "csv");
+        gpl.init("src/test/resources/NotExist", "csv");
         List<String> pathList = gpl.runCommand();
 
         assertEquals(1, gpl.getCode());
@@ -38,7 +36,7 @@ public class GetPathListTest {
 
     @Test
     public void test3() {
-        GetPathList gpl = new GetPathList("src/test/resources", "txt");
+        gpl.init("src/test/resources", "txt");
         List<String> pathList = gpl.runCommand();
 
         assertEquals(0, gpl.getCode());
