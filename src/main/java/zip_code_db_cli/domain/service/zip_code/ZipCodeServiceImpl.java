3c9cbdef085244a0899a5ec8037e708a146dfdcb
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-11-07 00:53:45 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
    ConnectionRepository, ContentsAttribute の使用を廃止。
    Connection を継承元である ZipCodeService から取得するように修正。

--- src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
+++ src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
@@ -4,24 +4,20 @@ import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
-import java_itamae_contents.domain.model.ContentsAttribute;
+import java_itamae_connection.domain.model.ConnectionInfo;
 import zip_code_db_cli.domain.model.ZipCode;
-import zip_code_db_cli.domain.repository.connection.ConnectionRepository;
-import zip_code_db_cli.domain.repository.connection.ConnectionRepositoryImpl;
 import zip_code_db_cli.domain.repository.zip_code.ZipCodeRepository;
 import zip_code_db_cli.domain.repository.zip_code.ZipCodeRepositoryImpl;
 
-public class ZipCodeServiceImpl implements ZipCodeService {
-    private final ContentsAttribute attr;
-    private final ConnectionRepository cr;
+public class ZipCodeServiceImpl extends ZipCodeService {
+    private final ConnectionInfo cnInfo;
     private final ZipCodeRepository zcr;
 
     /**
-     * @param attr DB の接続情報を記載したファイルの情報を納めた ConnectionAttibute を指定する。
+     * @param cnInfo DB の接続情報を納めた ConnectionInfo を指定する。
      */
-    public ZipCodeServiceImpl(ContentsAttribute attr) {
-        this.attr = attr;
-        cr = new ConnectionRepositoryImpl();
+    public ZipCodeServiceImpl(ConnectionInfo cnInfo) {
+        this.cnInfo = cnInfo;
         zcr = new ZipCodeRepositoryImpl();
     }
 
@@ -29,7 +25,7 @@ public class ZipCodeServiceImpl implements ZipCodeService {
     public List<ZipCode> findAll() throws Exception {
         List<ZipCode> recordset = new ArrayList<>();
 
-        try (Connection connection = cr.getConnection(attr)) {
+        try (Connection connection = getConnection(cnInfo)) {
             recordset = zcr.findAll(connection);
         }
 
@@ -40,7 +36,7 @@ public class ZipCodeServiceImpl implements ZipCodeService {
     public boolean create(List<ZipCode> recordset) throws Exception {
         boolean status = false;
 
-        try (Connection connection = cr.getConnection(attr)) {
+        try (Connection connection = getConnection(cnInfo)) {
             status = zcr.create(connection, recordset);
         }
 
@@ -51,7 +47,7 @@ public class ZipCodeServiceImpl implements ZipCodeService {
     public boolean deleteAll() throws Exception {
         boolean status = false;
 
-        try (Connection connection = cr.getConnection(attr)) {
+        try (Connection connection = getConnection(cnInfo)) {
             final List<ZipCode> recordset = zcr.findAll(connection);
 
             if (recordset.size() > 0) {
