d56a463edc5377ae54d2216b1ce8de872f384109
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-11-07 00:58:23 +0900

    renamed: src/test/java/zip_code_db_cli/domain/{repository/zip_code/ZipCodeRepositoryTest.java -> service/zip_code/ZipCodeServiceTest.java}
    ConnectionRepository の削除に伴うコード修正と
    テスト対象クラスの変更。

--- src/test/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceTest.java
+++ src/test/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceTest.java
@@ -1,9 +1,8 @@
-package zip_code_db_cli.domain.repository.zip_code;
+package zip_code_db_cli.domain.service.zip_code;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,48 +12,42 @@ import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
+import java_itamae_connection.domain.service.connection_info.ConnectionInfoService;
+import java_itamae_connection.domain.service.connection_info.ConnectionInfoServiceImpl;
 import java_itamae_contents.domain.model.ContentsAttribute;
 import zip_code_db_cli.domain.model.ZipCode;
-import zip_code_db_cli.domain.repository.connection.ConnectionRepository;
-import zip_code_db_cli.domain.repository.connection.ConnectionRepositoryImpl;
+import zip_code_db_cli.domain.service.zip_code.ZipCodeService;
+import zip_code_db_cli.domain.service.zip_code.ZipCodeServiceImpl;
 
 @RunWith(Enclosed.class)
-public class ZipCodeRepositoryTest {
+public class ZipCodeServiceTest {
     public static class テーブルが空である場合 {
-        private ContentsAttribute attr;
-        private ConnectionRepository cs;
-        private ZipCodeRepository zcr;
+        private ZipCodeService zcs;
 
         @Before
         public void setUp() throws Exception {
-            attr = new ContentsAttribute();
+            final ContentsAttribute attr = new ContentsAttribute();
             attr.setPath("src/test/resources/config.properties");
 
-            cs = new ConnectionRepositoryImpl();
-            zcr = new ZipCodeRepositoryImpl();
+            final ConnectionInfoService cis = new ConnectionInfoServiceImpl();
+            zcs = new ZipCodeServiceImpl(cis.getConnectionInfo(attr));
 
-            try (Connection connection = cs.getConnection(attr)) {
-                final List<ZipCode> recordset = zcr.findAll(connection);
+            final List<ZipCode> recordset = zcs.findAll();
 
-                if (recordset.size() > 0) {
-                    zcr.deleteAll(connection);
-                }
+            if (recordset.size() > 0) {
+                zcs.deleteAll();
             }
         }
 
         @After
         public void tearDown() throws Exception {
-            try (Connection connection = cs.getConnection(attr)) {
-                zcr.deleteAll(connection);
-            }
+            zcs.deleteAll();
         }
 
         @Test
         public void findAll実行時に空のリストが返されること() throws Exception {
-            try (Connection connection = cs.getConnection(attr)) {
-                final List<ZipCode> recordset = zcr.findAll(connection);
-                assertThat(recordset.size(), is(0));
-            }
+            final List<ZipCode> recordset = zcs.findAll();
+            assertThat(recordset.size(), is(0));
         }
 
         @Test
@@ -93,42 +86,36 @@ public class ZipCodeRepositoryTest {
                 contents.add(zipcode);
             }
 
-            try (Connection connection = cs.getConnection(attr)) {
-                final boolean status = zcr.create(connection, contents);
-                assertThat(status, is(true));
+            final boolean status = zcs.create(contents);
+            assertThat(status, is(true));
 
-                final List<ZipCode> recordset = zcr.findAll(connection);
-                assertThat(recordset.size(), is(2));
+            final List<ZipCode> recordset = zcs.findAll();
+            assertThat(recordset.size(), is(2));
 
-                assertThat(recordset.get(0).getZipCode(), is("0600000"));
-                assertThat(recordset.get(0).getPrefecture(), is("北海道"));
-                assertThat(recordset.get(0).getCity(), is("札幌市中央区"));
-                assertThat(recordset.get(0).getArea(), is("以下に掲載がない場合"));
+            assertThat(recordset.get(0).getZipCode(), is("0600000"));
+            assertThat(recordset.get(0).getPrefecture(), is("北海道"));
+            assertThat(recordset.get(0).getCity(), is("札幌市中央区"));
+            assertThat(recordset.get(0).getArea(), is("以下に掲載がない場合"));
 
-                assertThat(recordset.get(1).getZipCode(), is("0600042"));
-                assertThat(recordset.get(1).getPrefecture(), is("北海道"));
-                assertThat(recordset.get(1).getCity(), is("札幌市中央区"));
-                assertThat(recordset.get(1).getArea(), is("大通西（１～１９丁目）"));
-            }
+            assertThat(recordset.get(1).getZipCode(), is("0600042"));
+            assertThat(recordset.get(1).getPrefecture(), is("北海道"));
+            assertThat(recordset.get(1).getCity(), is("札幌市中央区"));
+            assertThat(recordset.get(1).getArea(), is("大通西（１～１９丁目）"));
         }
 
         public void create実行時に空のリストを渡した場合に終了ステータスfalseがであること() throws Exception {
             final List<ZipCode> contents = new ArrayList<>();
 
-            try (Connection connection = cs.getConnection(attr)) {
-                final boolean status = zcr.create(connection, contents);
-                assertThat(status, is(false));
+            final boolean status = zcs.create(contents);
+            assertThat(status, is(false));
 
-                final List<ZipCode> recordset = zcr.findAll(connection);
-                assertThat(recordset.size(), is(0));
-            }
+            final List<ZipCode> recordset = zcs.findAll();
+            assertThat(recordset.size(), is(0));
         }
     }
 
     public static class テーブルが空ではない場合 {
-        private ContentsAttribute attr;
-        private ConnectionRepository cs;
-        private ZipCodeRepository zcr;
+        private ZipCodeService zcs;
 
         @Before
         public void setUp() throws Exception {
@@ -166,46 +153,40 @@ public class ZipCodeRepositoryTest {
                 contents.add(zipcode);
             }
 
-            attr = new ContentsAttribute();
+            final ContentsAttribute attr = new ContentsAttribute();
             attr.setPath("src/test/resources/config.properties");
 
-            cs = new ConnectionRepositoryImpl();
-            zcr = new ZipCodeRepositoryImpl();
+            final ConnectionInfoService cis = new ConnectionInfoServiceImpl();
+            zcs = new ZipCodeServiceImpl(cis.getConnectionInfo(attr));
 
-            try (Connection connection = cs.getConnection(attr)) {
-                final List<ZipCode> recordset = zcr.findAll(connection);
+            final List<ZipCode> recordset = zcs.findAll();
 
-                if (recordset.size() > 0) {
-                    zcr.deleteAll(connection);
-                }
-
-                zcr.create(connection, contents);
+            if (recordset.size() > 0) {
+                zcs.deleteAll();
             }
+
+            zcs.create(contents);
         }
 
         @After
         public void tearDown() throws Exception {
-            try (Connection connection = cs.getConnection(attr)) {
-                zcr.deleteAll(connection);
-            }
+            zcs.deleteAll();
         }
 
         @Test
         public void findAll実行時にレコードがリストに変換されて返されること() throws Exception {
-            try (Connection connection = cs.getConnection(attr)) {
-                final List<ZipCode> recordset = zcr.findAll(connection);
-                assertThat(recordset.size(), is(2));
+            final List<ZipCode> recordset = zcs.findAll();
+            assertThat(recordset.size(), is(2));
 
-                assertThat(recordset.get(0).getZipCode(), is("0600000"));
-                assertThat(recordset.get(0).getPrefecture(), is("北海道"));
-                assertThat(recordset.get(0).getCity(), is("札幌市中央区"));
-                assertThat(recordset.get(0).getArea(), is("以下に掲載がない場合"));
+            assertThat(recordset.get(0).getZipCode(), is("0600000"));
+            assertThat(recordset.get(0).getPrefecture(), is("北海道"));
+            assertThat(recordset.get(0).getCity(), is("札幌市中央区"));
+            assertThat(recordset.get(0).getArea(), is("以下に掲載がない場合"));
 
-                assertThat(recordset.get(1).getZipCode(), is("0600042"));
-                assertThat(recordset.get(1).getPrefecture(), is("北海道"));
-                assertThat(recordset.get(1).getCity(), is("札幌市中央区"));
-                assertThat(recordset.get(1).getArea(), is("大通西（１～１９丁目）"));
-            }
+            assertThat(recordset.get(1).getZipCode(), is("0600042"));
+            assertThat(recordset.get(1).getPrefecture(), is("北海道"));
+            assertThat(recordset.get(1).getCity(), is("札幌市中央区"));
+            assertThat(recordset.get(1).getArea(), is("大通西（１～１９丁目）"));
         }
 
         @Test
@@ -228,28 +209,24 @@ public class ZipCodeRepositoryTest {
                 contents.add(zipcode);
             }
 
-            try (Connection connection = cs.getConnection(attr)) {
-                final boolean status = zcr.create(connection, contents);
-                assertThat(status, is(true));
+            final boolean status = zcs.create(contents);
+            assertThat(status, is(true));
 
-                final List<ZipCode> recordset = zcr.findAll(connection);
-                assertThat(recordset.size(), is(3));
+            final List<ZipCode> recordset = zcs.findAll();
+            assertThat(recordset.size(), is(3));
 
-                assertThat(recordset.get(2).getZipCode(), is("0028071"));
-                assertThat(recordset.get(2).getPrefecture(), is("北海道"));
-                assertThat(recordset.get(2).getCity(), is("札幌市北区"));
-                assertThat(recordset.get(2).getArea(), is("あいの里一条"));
-            }
+            assertThat(recordset.get(2).getZipCode(), is("0028071"));
+            assertThat(recordset.get(2).getPrefecture(), is("北海道"));
+            assertThat(recordset.get(2).getCity(), is("札幌市北区"));
+            assertThat(recordset.get(2).getArea(), is("あいの里一条"));
         }
 
         public void deleteAll実行時にテーブルを空にできること() throws Exception {
-            try (Connection connection = cs.getConnection(attr)) {
-                final boolean status = zcr.deleteAll(connection);
-                assertThat(status, is(true));
+            final boolean status = zcs.deleteAll();
+            assertThat(status, is(true));
 
-                final List<ZipCode> recordset = zcr.findAll(connection);
-                assertThat(recordset.size(), is(0));
-            }
+            final List<ZipCode> recordset = zcs.findAll();
+            assertThat(recordset.size(), is(0));
         }
     }
 }
