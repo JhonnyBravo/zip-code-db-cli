7572b8691800b9723e3d25a24049b183b83f61f0
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-12-02 21:43:04 +0900

    modified: src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
    MyBatis の導入に伴うコード修正。

--- src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
+++ src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
@@ -1,42 +1,55 @@
 package zip_code_db_cli.domain.repository.zip_code;
 
-import java.sql.Connection;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import zip_code_db_cli.domain.model.ZipCode;
 
+@Mapper
 public interface ZipCodeRepository {
     /**
-     * @param connection DB 接続に使用する Connection を指定する。
      * @return recordset テーブルからレコードを全件取得する。
-     * @throws Exception {@link java.lang.Exception}
+     * @throws Exception
+     *             {@link java.lang.Exception}
      */
-    public List<ZipCode> findAll(Connection connection) throws Exception;
+    public List<ZipCode> findAll() throws Exception;
 
     /**
      * テーブルへレコードを一括登録する。
      *
-     * @param connection DB 接続に使用する Connection を指定する。
-     * @param contents   登録対象とするレコードセットを指定する。
+     * @param contents
+     *            登録対象とするレコードセットを指定する。
      * @return status
      *         <ul>
      *         <li>true: レコードの登録に成功したことを表す。</li>
-     *         <li>false: レコードの登録が失敗したことを表す。</li>
+     *         <li>false: レコードが登録されなかったことを表す。</li>
      *         </ul>
-     * @throws Exception {@link java.lang.Exception}
+     * @throws Exception
+     *             {@link java.lang.Exception}
      */
-    public boolean create(Connection connection, List<ZipCode> contents) throws Exception;
+    public boolean create(@Param("list") List<ZipCode> contents) throws Exception;
 
     /**
      * テーブルからレコードを全件削除する。
      *
-     * @param connection DB 接続に使用する Connection を指定する。
      * @return status
      *         <ul>
      *         <li>true: レコードの削除に成功したことを表す。</li>
-     *         <li>false: レコードの削除が失敗したことを表す。</li>
+     *         <li>false: レコードが削除されなかったことを表す。</li>
      *         </ul>
-     * @throws Exception {@link java.lang.Exception}
+     * @throws Exception
+     *             {@link java.lang.Exception}
      */
-    public boolean deleteAll(Connection connection) throws Exception;
+    public boolean deleteAll() throws Exception;
+
+    /**
+     * ID のカウンターを 1 にリセットする。
+     * 
+     * @return status
+     * @throws Exception
+     *             {@link java.lang.Exception}
+     */
+    public boolean resetId() throws Exception;
 }
