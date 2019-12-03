e1076b30385376c0238d6b49683a9038d6347318
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-12-03 20:21:28 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
    MyBatis を導入。

--- src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
+++ src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
@@ -1,32 +1,32 @@
 package zip_code_db_cli.domain.service.zip_code;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
-import java_itamae_connection.domain.model.ConnectionInfo;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import zip_code_db_cli.domain.model.ZipCode;
 import zip_code_db_cli.domain.repository.zip_code.ZipCodeRepository;
-import zip_code_db_cli.domain.repository.zip_code.ZipCodeRepositoryImpl;
 
-public class ZipCodeServiceImpl extends ZipCodeService {
-    private final ConnectionInfo cnInfo;
-    private final ZipCodeRepository zcr;
+public class ZipCodeServiceImpl implements ZipCodeService {
+    private final SqlSessionFactory factory;
+    private ZipCodeRepository repository;
 
-    /**
-     * @param cnInfo DB の接続情報を納めた ConnectionInfo を指定する。
-     */
-    public ZipCodeServiceImpl(ConnectionInfo cnInfo) {
-        this.cnInfo = cnInfo;
-        zcr = new ZipCodeRepositoryImpl();
+    public ZipCodeServiceImpl() throws Exception {
+        factory = new SqlSessionFactoryBuilder()
+                .build(Resources.getResourceAsStream("mybatis-config.xml"));
     }
 
     @Override
     public List<ZipCode> findAll() throws Exception {
         List<ZipCode> recordset = new ArrayList<>();
 
-        try (Connection connection = getConnection(cnInfo)) {
-            recordset = zcr.findAll(connection);
+        try (SqlSession session = factory.openSession()) {
+            repository = session.getMapper(ZipCodeRepository.class);
+            recordset = repository.findAll();
         }
 
         return recordset;
@@ -36,8 +36,12 @@ public class ZipCodeServiceImpl extends ZipCodeService {
     public boolean create(List<ZipCode> recordset) throws Exception {
         boolean status = false;
 
-        try (Connection connection = getConnection(cnInfo)) {
-            status = zcr.create(connection, recordset);
+        if (recordset.size() > 0) {
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                status = repository.create(recordset);
+                session.commit();
+            }
         }
 
         return status;
@@ -47,12 +51,11 @@ public class ZipCodeServiceImpl extends ZipCodeService {
     public boolean deleteAll() throws Exception {
         boolean status = false;
 
-        try (Connection connection = getConnection(cnInfo)) {
-            final List<ZipCode> recordset = zcr.findAll(connection);
-
-            if (recordset.size() > 0) {
-                status = zcr.deleteAll(connection);
-            }
+        try (SqlSession session = factory.openSession()) {
+            repository = session.getMapper(ZipCodeRepository.class);
+            status = repository.deleteAll();
+            repository.resetId();
+            session.commit();
         }
 
         return status;
