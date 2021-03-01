94c34622f247b25088190fcabcd458609a99d231
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-03-02 01:52:23 +0900

    modified: src/test/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceTest.java
    パッケージのバージョンアップに伴う import の修正。

--- src/test/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceTest.java
+++ src/test/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceTest.java
@@ -1,7 +1,8 @@
 package zip_code_db_cli.domain.service.zip_code;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
