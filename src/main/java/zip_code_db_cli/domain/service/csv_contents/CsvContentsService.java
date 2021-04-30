58cdfb787ad0d04df588bbbaaadee08fbd3784f3
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-01 00:11:46 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/csv_contents/CsvContentsService.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/zip_code_db_cli/domain/service/csv_contents/CsvContentsService.java
+++ src/main/java/zip_code_db_cli/domain/service/csv_contents/CsvContentsService.java
@@ -1,53 +1,55 @@
 package zip_code_db_cli.domain.service.csv_contents;
 
 import java.util.List;
-
 import zip_code_db_cli.domain.model.ZipCode;
 
+/**
+ * CSV の読み書きを操作する。
+ */
 public interface CsvContentsService {
-    /**
-     * CSV を読み込んで List に変換して返す。
-     *
-     * @return contents 変換された List
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public List<ZipCode> getContents() throws Exception;
+  /**
+   * CSV を読み込んで {@link ZipCode} の {@link List} に変換して返す。
+   *
+   * @return contents {@link ZipCode} の {@link List} を返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  List<ZipCode> getContents() throws Exception;
 
-    /**
-     * CSV を上書きする。
-     *
-     * @param content 書込み対象とする情報を納めた ZipCode を指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: 書込みに成功したことを表す。</li>
-     *         <li>false: 書込みを実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public boolean setContent(ZipCode content) throws Exception;
+  /**
+   * CSV を上書きする。
+   *
+   * @param content 書込み対象とする情報を納めた {@link ZipCode} を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: 書込みに成功したことを表す。</li>
+   *         <li>false: 書込みを実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean updateContent(ZipCode content) throws Exception;
 
-    /**
-     * CSV を一行追記する。
-     *
-     * @param content 書込み対象とする情報を納めた ZipCode を指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: 追記に成功したことを表す。</li>
-     *         <li>false: 追記を実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public boolean appendContent(ZipCode content) throws Exception;
+  /**
+   * CSV を一行追記する。
+   *
+   * @param content 書込み対象とする情報を納めた {@link ZipCode} を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: 追記に成功したことを表す。</li>
+   *         <li>false: 追記を実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean appendContent(ZipCode content) throws Exception;
 
-    /**
-     * CSV を空にする。
-     *
-     * @return status
-     *         <ul>
-     *         <li>true: 削除に成功したことを表す。</li>
-     *         <li>false: 削除を実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public boolean deleteContents() throws Exception;
+  /**
+   * CSV を空にする。
+   *
+   * @return status
+   *         <ul>
+   *         <li>true: 削除に成功したことを表す。</li>
+   *         <li>false: 削除を実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean deleteContents() throws Exception;
 }
