c868459fdc59988ee3062baaf8071117488110de
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-01 00:14:40 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
+++ src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
@@ -1,39 +1,43 @@
 package zip_code_db_cli.domain.service.zip_code;
 
 import java.util.List;
-
 import java_itamae_connection.domain.service.connection.ConnectionService;
 import zip_code_db_cli.domain.model.ZipCode;
 
+/**
+ * t_zip_code へのレコードの読み書きを操作する。
+ */
 public abstract class ZipCodeService extends ConnectionService {
-    /**
-     * @return テーブルからレコードを全件取得する。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public abstract List<ZipCode> findAll() throws Exception;
+  /**
+   * テーブルに登録されている全レコードを取得する。
+   *
+   * @return {@link ZipCode} の {@link List} を返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public abstract List<ZipCode> findAll() throws Exception;
 
-    /**
-     * テーブルへレコードを一括登録する。
-     *
-     * @param recordset 登録対象とするレコードセットを指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: 登録に成功したことを表す。</li>
-     *         <li>false: 登録に失敗したことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public abstract boolean create(List<ZipCode> recordset) throws Exception;
+  /**
+   * テーブルへレコードを一括登録する。
+   *
+   * @param recordset 登録対象とする {@link ZipCode} の {@link List} を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: 登録に成功したことを表す。</li>
+   *         <li>false: 登録に失敗したことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public abstract boolean create(List<ZipCode> recordset) throws Exception;
 
-    /**
-     * テーブルからレコードを全件削除する。
-     *
-     * @return status
-     *         <ul>
-     *         <li>true: 削除に成功したことを表す。</li>
-     *         <li>false: 削除を実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public abstract boolean deleteAll() throws Exception;
+  /**
+   * テーブルからレコードを全件削除する。
+   *
+   * @return status
+   *         <ul>
+   *         <li>true: 削除に成功したことを表す。</li>
+   *         <li>false: 削除を実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public abstract boolean deleteAll() throws Exception;
 }
