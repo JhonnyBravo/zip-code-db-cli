33ccc7e1ba02b6a8dc891ab30207130306c261a6
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-04 00:53:08 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
+++ src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
@@ -2,62 +2,60 @@ package zip_code_db_cli.domain.service.zip_code;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
 import zip_code_db_cli.domain.model.ZipCode;
 import zip_code_db_cli.domain.repository.zip_code.ZipCodeRepository;
 
 public class ZipCodeServiceImpl implements ZipCodeService {
-    private final SqlSessionFactory factory;
-    private ZipCodeRepository repository;
+  private final SqlSessionFactory factory;
+  private ZipCodeRepository repository;
 
-    public ZipCodeServiceImpl() throws Exception {
-        factory = new SqlSessionFactoryBuilder()
-                .build(Resources.getResourceAsStream("mybatis-config.xml"));
+  public ZipCodeServiceImpl() throws Exception {
+    factory =
+        new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
+  }
+
+  @Override
+  public List<ZipCode> findAll() throws Exception {
+    List<ZipCode> recordset = new ArrayList<>();
+
+    try (SqlSession session = factory.openSession()) {
+      repository = session.getMapper(ZipCodeRepository.class);
+      recordset = repository.findAll();
     }
 
-    @Override
-    public List<ZipCode> findAll() throws Exception {
-        List<ZipCode> recordset = new ArrayList<>();
+    return recordset;
+  }
 
-        try (SqlSession session = factory.openSession()) {
-            repository = session.getMapper(ZipCodeRepository.class);
-            recordset = repository.findAll();
-        }
+  @Override
+  public boolean create(List<ZipCode> recordset) throws Exception {
+    boolean status = false;
 
-        return recordset;
+    if (recordset.size() > 0) {
+      try (SqlSession session = factory.openSession()) {
+        repository = session.getMapper(ZipCodeRepository.class);
+        status = repository.create(recordset);
+        session.commit();
+      }
     }
 
-    @Override
-    public boolean create(List<ZipCode> recordset) throws Exception {
-        boolean status = false;
+    return status;
+  }
 
-        if (recordset.size() > 0) {
-            try (SqlSession session = factory.openSession()) {
-                repository = session.getMapper(ZipCodeRepository.class);
-                status = repository.create(recordset);
-                session.commit();
-            }
-        }
+  @Override
+  public boolean deleteAll() throws Exception {
+    boolean status = false;
 
-        return status;
+    try (SqlSession session = factory.openSession()) {
+      repository = session.getMapper(ZipCodeRepository.class);
+      status = repository.deleteAll();
+      repository.resetId();
+      session.commit();
     }
 
-    @Override
-    public boolean deleteAll() throws Exception {
-        boolean status = false;
-
-        try (SqlSession session = factory.openSession()) {
-            repository = session.getMapper(ZipCodeRepository.class);
-            status = repository.deleteAll();
-            repository.resetId();
-            session.commit();
-        }
-
-        return status;
-    }
+    return status;
+  }
 }
