23e47f4fda9fe136ac85f3534ccab446e3944536
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-04 00:51:58 +0900

    modified: src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
+++ src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
@@ -1,55 +1,53 @@
 package zip_code_db_cli.domain.repository.zip_code;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-
 import zip_code_db_cli.domain.model.ZipCode;
 
+/**
+ * t_zip_code へのレコードの読み書きを操作する。
+ */
 @Mapper
 public interface ZipCodeRepository {
-    /**
-     * @return recordset テーブルからレコードを全件取得する。
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public List<ZipCode> findAll() throws Exception;
+  /**
+   * テーブルに登録されている全レコードを取得する。
+   *
+   * @return recordset {@link ZipCode} の {@link List} を返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  List<ZipCode> findAll() throws Exception;
 
-    /**
-     * テーブルへレコードを一括登録する。
-     *
-     * @param contents
-     *            登録対象とするレコードセットを指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: レコードの登録に成功したことを表す。</li>
-     *         <li>false: レコードが登録されなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public boolean create(@Param("list") List<ZipCode> contents) throws Exception;
+  /**
+   * テーブルへレコードを一括登録する。
+   *
+   * @param contents 登録対象とする {@link ZipCode} の {@link List} を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: レコードの登録に成功したことを表す。</li>
+   *         <li>false: レコードが登録されなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean create(@Param("list") List<ZipCode> contents) throws Exception;
 
-    /**
-     * テーブルからレコードを全件削除する。
-     *
-     * @return status
-     *         <ul>
-     *         <li>true: レコードの削除に成功したことを表す。</li>
-     *         <li>false: レコードが削除されなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public boolean deleteAll() throws Exception;
+  /**
+   * テーブルからレコードを全件削除する。
+   *
+   * @return status
+   *         <ul>
+   *         <li>true: レコードの削除に成功したことを表す。</li>
+   *         <li>false: レコードが削除されなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean deleteAll() throws Exception;
 
-    /**
-     * ID のカウンターを 1 にリセットする。
-     * 
-     * @return status
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public boolean resetId() throws Exception;
+  /**
+   * ID のカウンターを 1 にリセットする。
+   *
+   * @return status
+   * @throws Exception {@link java.lang.Exception}
+   */
+  boolean resetId() throws Exception;
 }
