168ea24362562f76fa3b6bcbb49bb9bc43724dc1
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-06 00:59:04 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/csv_contents/CsvContentsService.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/zip_code_db_cli/domain/service/csv_contents/CsvContentsService.java
+++ src/main/java/zip_code_db_cli/domain/service/csv_contents/CsvContentsService.java
@@ -1,59 +1,63 @@
 package zip_code_db_cli.domain.service.csv_contents;
 
 import java.util.List;
-
 import java_itamae_contents.domain.model.ContentsAttribute;
 import zip_code_db_cli.domain.model.ZipCodeCsvEntity;
 
+/**
+ * CSV の読み書きを操作する。
+ */
 public interface CsvContentsService {
-    /**
-     * @param attr 操作対象とするファイルの情報を収めた {@link ContentsAttribute} を指定する。
-     */
-    void init(ContentsAttribute attr);
+  /**
+   * 初期化処理を実行する。
+   *
+   * @param attr 操作対象とするファイルの情報を収めた {@link ContentsAttribute} を指定する。
+   */
+  void init(ContentsAttribute attr);
 
-    /**
-     * CSV を読込んで {@link List} に変換して返す。
-     *
-     * @return contents 変換された {@link List}
-     * @throws Exception {@link Exception}
-     */
-    List<ZipCodeCsvEntity> getContents() throws Exception;
+  /**
+   * CSV を読込んで {@link ZipCodeCsvEntity} の {@link List} に変換して返す。
+   *
+   * @return contents {@link ZipCodeCsvEntity} の {@link List}
+   * @throws Exception {@link Exception}
+   */
+  List<ZipCodeCsvEntity> getContents() throws Exception;
 
-    /**
-     * CSV を上書きする。
-     *
-     * @param content 書込み対象とする情報を収めた {@link ZipCodeCsvEntity} を指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: 書込みに成功したことを表す。</li>
-     *         <li>false: 書込みを実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link Exception}
-     */
-    boolean setContent(ZipCodeCsvEntity content) throws Exception;
+  /**
+   * CSV を上書きする。
+   *
+   * @param content 書込み対象とする情報を収めた {@link ZipCodeCsvEntity} を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: 書込みに成功したことを表す。</li>
+   *         <li>false: 書込みを実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link Exception}
+   */
+  boolean updateContent(ZipCodeCsvEntity content) throws Exception;
 
-    /**
-     * CSV を一行追記する。
-     *
-     * @param content 書込み対象とする情報を収めた {@link ZipCodeCsvEntity} を指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: 書込みに成功したことを表す。</li>
-     *         <li>false: 書込みを実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link Exception}
-     */
-    boolean appendContent(ZipCodeCsvEntity content) throws Exception;
+  /**
+   * CSV を一行追記する。
+   *
+   * @param content 書込み対象とする情報を収めた {@link ZipCodeCsvEntity} を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: 書込みに成功したことを表す。</li>
+   *         <li>false: 書込みを実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link Exception}
+   */
+  boolean appendContent(ZipCodeCsvEntity content) throws Exception;
 
-    /**
-     * CSV を空にする。
-     *
-     * @return status
-     *         <ul>
-     *         <li>true: 削除に成功したことを表す。</li>
-     *         <li>false: 削除を実行しなかったことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link Exception}
-     */
-    boolean deleteContents() throws Exception;
+  /**
+   * CSV を空にする。
+   *
+   * @return status
+   *         <ul>
+   *         <li>true: 削除に成功したことを表す。</li>
+   *         <li>false: 削除を実行しなかったことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link Exception}
+   */
+  boolean deleteContents() throws Exception;
 }
