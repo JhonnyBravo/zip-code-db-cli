e39b5e5f5136c5001d011e72973ac0e9fcad6810
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-06 01:01:29 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
+++ src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
@@ -1,38 +1,42 @@
 package zip_code_db_cli.domain.service.zip_code;
 
 import java.util.List;
-
 import zip_code_db_cli.domain.model.ZipCodeEntity;
 
+/**
+ * zip_code テーブルへのレコードの読み書きを操作する。
+ */
 public interface ZipCodeService {
-    /**
-     * @return recordset テーブルからレコードを全件取得し、 ID 昇順に並べ替えて返す。
-     * @throws Exception {@link Exception}
-     */
-    List<ZipCodeEntity> findAllByOrderById() throws Exception;
+  /**
+   * テーブルに登録されている全レコードを取得し、 ID 昇順に並べ替えて返す。
+   *
+   * @return recordset {@link ZipCodeEntity} の {@link List}
+   * @throws Exception {@link Exception}
+   */
+  List<ZipCodeEntity> findAllByOrderById() throws Exception;
 
-    /**
-     * テーブルへレコードを一括登録する。
-     *
-     * @param recordset 登録対象とするレコードセットを指定する。
-     * @return status
-     *         <ul>
-     *         <li>true: 登録に成功したことを表す。</li>
-     *         <li>false: 登録に失敗したことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link Exception}
-     */
-    boolean saveAll(List<ZipCodeEntity> recordset) throws Exception;
+  /**
+   * テーブルへレコードを一括登録する。
+   *
+   * @param recordset 登録対象とする {@link ZipCodeEntity} の {@link List} を指定する。
+   * @return status
+   *         <ul>
+   *         <li>true: 登録に成功したことを表す。</li>
+   *         <li>false: 登録に失敗したことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link Exception}
+   */
+  boolean saveAll(List<ZipCodeEntity> recordset) throws Exception;
 
-    /**
-     * テーブルからレコードを全件削除する。
-     *
-     * @return status
-     *         <ul>
-     *         <li>true: 削除に成功したことを表す。</li>
-     *         <li>false: 削除に失敗したことを表す。</li>
-     *         </ul>
-     * @throws Exception {@link Exception}
-     */
-    boolean deleteAll() throws Exception;
+  /**
+   * テーブルからレコードを全件削除する。
+   *
+   * @return status
+   *         <ul>
+   *         <li>true: 削除に成功したことを表す。</li>
+   *         <li>false: 削除に失敗したことを表す。</li>
+   *         </ul>
+   * @throws Exception {@link Exception}
+   */
+  boolean deleteAll() throws Exception;
 }
