0220be9e17695507a629ee8c17444a899786217a
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-06 00:58:18 +0900

    modified: src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
+++ src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
@@ -1,10 +1,8 @@
 package zip_code_db_cli.domain.repository.zip_code;
 
 import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import zip_code_db_cli.domain.model.ZipCodeEntity;
 
 /**
@@ -12,8 +10,10 @@ import zip_code_db_cli.domain.model.ZipCodeEntity;
  */
 @Repository
 public interface ZipCodeRepository extends CrudRepository<ZipCodeEntity, Integer> {
-    /**
-     * @return recordset 登録されているレコードを全件取得し、 ID 昇順に並べ替えて返す。
-     */
-    List<ZipCodeEntity> findAllByOrderById();
+  /**
+   * 登録されている全レコードを取得し、 ID 昇順に並べ替えて返す。
+   *
+   * @return recordset {@link ZipCodeEntity} の {@link List}
+   */
+  List<ZipCodeEntity> findAllByOrderById();
 }
