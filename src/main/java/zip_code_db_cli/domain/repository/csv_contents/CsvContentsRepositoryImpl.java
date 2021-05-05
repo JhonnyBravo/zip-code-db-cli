f982b82902cde21f9ba5e6e0c125ac02a14ec076
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-06 00:55:17 +0900

    modified: src/main/java/zip_code_db_cli/domain/repository/csv_contents/CsvContentsRepositoryImpl.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/zip_code_db_cli/domain/repository/csv_contents/CsvContentsRepositoryImpl.java
+++ src/main/java/zip_code_db_cli/domain/repository/csv_contents/CsvContentsRepositoryImpl.java
@@ -1,45 +1,43 @@
 package zip_code_db_cli.domain.repository.csv_contents;
 
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import java.io.Reader;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
-import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-
 import zip_code_db_cli.domain.model.ZipCodeCsvEntity;
 
 @Service
 public class CsvContentsRepositoryImpl implements CsvContentsRepository {
 
-    @Override
-    public List<ZipCodeCsvEntity> getContents(Reader reader) throws Exception {
-        List<ZipCodeCsvEntity> contents = new ArrayList<>();
-        contents = new CsvToBeanBuilder<ZipCodeCsvEntity>(reader).withType(ZipCodeCsvEntity.class).build().parse();
-        return contents;
-    }
+  @Override
+  public List<ZipCodeCsvEntity> getContents(Reader reader) throws Exception {
+    List<ZipCodeCsvEntity> contents = new ArrayList<>();
+    contents = new CsvToBeanBuilder<ZipCodeCsvEntity>(reader).withType(ZipCodeCsvEntity.class)
+        .build().parse();
+    return contents;
+  }
 
-    @Override
-    public boolean setContents(Writer writer, List<ZipCodeCsvEntity> contents) throws Exception {
-        boolean status = false;
+  @Override
+  public boolean updateContents(Writer writer, List<ZipCodeCsvEntity> contents) throws Exception {
+    boolean status = false;
 
-        new StatefulBeanToCsvBuilder<ZipCodeCsvEntity>(writer).build().write(contents);
-        status = true;
+    new StatefulBeanToCsvBuilder<ZipCodeCsvEntity>(writer).build().write(contents);
+    status = true;
 
-        return status;
-    }
+    return status;
+  }
 
-    @Override
-    public boolean deleteContents(Writer writer) throws Exception {
-        boolean status = false;
+  @Override
+  public boolean deleteContents(Writer writer) throws Exception {
+    boolean status = false;
 
-        final List<ZipCodeCsvEntity> contents = new ArrayList<>();
-        status = setContents(writer, contents);
+    final List<ZipCodeCsvEntity> contents = new ArrayList<>();
+    status = updateContents(writer, contents);
 
-        return status;
-    }
+    return status;
+  }
 
 }
