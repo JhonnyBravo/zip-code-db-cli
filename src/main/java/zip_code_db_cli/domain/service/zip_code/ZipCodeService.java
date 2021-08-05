528527f7d95a6def75fd10c9cc0711746db80c0f
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-08-06 01:23:55 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
    new function: init

--- src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
+++ src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
@@ -1,6 +1,7 @@
 package zip_code_db_cli.domain.service.zip_code;
 
 import java.util.List;
+import java_itamae_connection.domain.model.ConnectionInfo;
 import java_itamae_connection.domain.service.connection.ConnectionService;
 import zip_code_db_cli.domain.model.ZipCode;
 
@@ -8,6 +9,14 @@ import zip_code_db_cli.domain.model.ZipCode;
  * t_zip_code へのレコードの読み書きを操作する。
  */
 public abstract class ZipCodeService extends ConnectionService {
+  /**
+   * 初期化処理を実行する。
+   *
+   * @param cnInfo DB の接続情報を納めた {@link ConnectionInfo} を指定する。
+   * @throws Exception {@link Exception}}
+   */
+  public abstract void init(ConnectionInfo cnInfo) throws Exception;
+
   /**
    * テーブルに登録されている全レコードを取得する。
    *
