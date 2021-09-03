069b256bfa8682a9d050d75c232b9f0c29f1a575
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-09-04 02:38:26 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
    JPA の導入に伴うコード修正。

--- src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
+++ src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeServiceImpl.java
@@ -1,42 +1,25 @@
 package zip_code_db_cli.domain.service.zip_code;
 
 import jakarta.inject.Inject;
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
-import java_itamae_connection.domain.model.ConnectionInfo;
-import zip_code_db_cli.domain.model.ZipCode;
+import zip_code_db_cli.domain.model.ZipCodeEntity;
 import zip_code_db_cli.domain.repository.zip_code.ZipCodeRepository;
 
-public class ZipCodeServiceImpl extends ZipCodeService {
+public class ZipCodeServiceImpl implements ZipCodeService {
   @Inject
-  private ZipCodeRepository zcr;
-  private ConnectionInfo cnInfo;
+  private ZipCodeRepository repository;
 
   @Override
-  public void init(ConnectionInfo cnInfo) throws Exception {
-    this.cnInfo = cnInfo;
-  }
-
-  @Override
-  public List<ZipCode> findAll() throws Exception {
-    List<ZipCode> recordset = new ArrayList<>();
-
-    try (Connection connection = getConnection(cnInfo)) {
-      recordset = zcr.findAll(connection);
-    }
-
+  public List<ZipCodeEntity> findAll() throws Exception {
+    List<ZipCodeEntity> recordset = new ArrayList<>();
+    recordset = repository.findAll();
     return recordset;
   }
 
   @Override
-  public boolean create(List<ZipCode> recordset) throws Exception {
-    boolean status = false;
-
-    try (Connection connection = getConnection(cnInfo)) {
-      status = zcr.create(connection, recordset);
-    }
-
+  public boolean create(List<ZipCodeEntity> recordset) throws Exception {
+    final boolean status = repository.create(recordset);
     return status;
   }
 
@@ -44,12 +27,10 @@ public class ZipCodeServiceImpl extends ZipCodeService {
   public boolean deleteAll() throws Exception {
     boolean status = false;
 
-    try (Connection connection = getConnection(cnInfo)) {
-      final List<ZipCode> recordset = zcr.findAll(connection);
+    final List<ZipCodeEntity> recordset = repository.findAll();
 
-      if (recordset.size() > 0) {
-        status = zcr.deleteAll(connection);
-      }
+    if (recordset.size() > 0) {
+      status = repository.deleteAll();
     }
 
     return status;
