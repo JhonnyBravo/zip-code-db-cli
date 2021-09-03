85349d9e2ea0e7419bc1c2653a09d5532db7c044
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-09-04 02:29:26 +0900

    modified: src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
    JPA の導入に伴うコード修正。

--- src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
+++ src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
@@ -1,8 +1,7 @@
 package zip_code_db_cli.domain.repository.zip_code;
 
-import java.sql.Connection;
 import java.util.List;
-import zip_code_db_cli.domain.model.ZipCode;
+import zip_code_db_cli.domain.model.ZipCodeEntity;
 
 /**
  * t_zip_code へのレコードの読み書きを操作する。
@@ -11,17 +10,15 @@ public interface ZipCodeRepository {
   /**
    * テーブルに登録されている全レコードを取得する。
    *
-   * @param connection DB 接続に使用する {@link Connection} を指定する。
-   * @return recordset {@link ZipCode} の {@link List} を返す。
+   * @return recordset {@link ZipCodeEntity} の {@link List} を返す。
    * @throws Exception {@link java.lang.Exception}
    */
-  List<ZipCode> findAll(Connection connection) throws Exception;
+  List<ZipCodeEntity> findAll() throws Exception;
 
   /**
    * テーブルへレコードを一括登録する。
    *
-   * @param connection DB 接続に使用する {@link Connection} を指定する。
-   * @param contents 登録対象とする {@link ZipCode} の {@link List} を指定する。
+   * @param recordset 登録対象とする {@link ZipCodeEntity} の {@link List} を指定する。
    * @return status
    *         <ul>
    *         <li>true: レコードの登録に成功したことを表す。</li>
@@ -29,12 +26,11 @@ public interface ZipCodeRepository {
    *         </ul>
    * @throws Exception {@link java.lang.Exception}
    */
-  boolean create(Connection connection, List<ZipCode> contents) throws Exception;
+  boolean create(List<ZipCodeEntity> recordset) throws Exception;
 
   /**
    * テーブルからレコードを全件削除する。
    *
-   * @param connection DB 接続に使用する {@link Connection} を指定する。
    * @return status
    *         <ul>
    *         <li>true: レコードの削除に成功したことを表す。</li>
@@ -42,5 +38,5 @@ public interface ZipCodeRepository {
    *         </ul>
    * @throws Exception {@link java.lang.Exception}
    */
-  boolean deleteAll(Connection connection) throws Exception;
+  boolean deleteAll() throws Exception;
 }
