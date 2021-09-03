1756b45f5c04f091b29a7ab7ef99498664ec53c4
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-09-04 02:37:47 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
    JPA の導入に伴うコード修正。

--- src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
+++ src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
@@ -1,34 +1,24 @@
 package zip_code_db_cli.domain.service.zip_code;
 
 import java.util.List;
-import java_itamae_connection.domain.model.ConnectionInfo;
-import java_itamae_connection.domain.service.connection.ConnectionService;
-import zip_code_db_cli.domain.model.ZipCode;
+import zip_code_db_cli.domain.model.ZipCodeEntity;
 
 /**
  * t_zip_code へのレコードの読み書きを操作する。
  */
-public abstract class ZipCodeService extends ConnectionService {
-  /**
-   * 初期化処理を実行する。
-   *
-   * @param cnInfo DB の接続情報を納めた {@link ConnectionInfo} を指定する。
-   * @throws Exception {@link Exception}}
-   */
-  public abstract void init(ConnectionInfo cnInfo) throws Exception;
-
+public interface ZipCodeService {
   /**
    * テーブルに登録されている全レコードを取得する。
    *
-   * @return {@link ZipCode} の {@link List} を返す。
+   * @return {@link ZipCodeEntity} の {@link List} を返す。
    * @throws Exception {@link java.lang.Exception}
    */
-  public abstract List<ZipCode> findAll() throws Exception;
+  List<ZipCodeEntity> findAll() throws Exception;
 
   /**
    * テーブルへレコードを一括登録する。
    *
-   * @param recordset 登録対象とする {@link ZipCode} の {@link List} を指定する。
+   * @param recordset 登録対象とする {@link ZipCodeEntity} の {@link List} を指定する。
    * @return status
    *         <ul>
    *         <li>true: 登録に成功したことを表す。</li>
@@ -36,7 +26,7 @@ public abstract class ZipCodeService extends ConnectionService {
    *         </ul>
    * @throws Exception {@link java.lang.Exception}
    */
-  public abstract boolean create(List<ZipCode> recordset) throws Exception;
+  boolean create(List<ZipCodeEntity> recordset) throws Exception;
 
   /**
    * テーブルからレコードを全件削除する。
@@ -48,5 +38,5 @@ public abstract class ZipCodeService extends ConnectionService {
    *         </ul>
    * @throws Exception {@link java.lang.Exception}
    */
-  public abstract boolean deleteAll() throws Exception;
+  boolean deleteAll() throws Exception;
 }
