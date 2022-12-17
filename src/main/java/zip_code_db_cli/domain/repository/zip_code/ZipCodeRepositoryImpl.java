003a547789e475fe1286fa987a339311b095aa68
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-12-18 01:40:41 +0900

    modified: src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepositoryImpl.java
    ZipCodeRepository の仕様変更に伴うコードの修正。

--- src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepositoryImpl.java
+++ src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepositoryImpl.java
@@ -5,16 +5,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java_itamae_connection.domain.model.ConnectionInfo;
 import zip_code_db_cli.domain.model.ZipCode;
 
 public class ZipCodeRepositoryImpl implements ZipCodeRepository {
+  /** {@link ConnectionInfo} のインスタンス */
+  private transient ConnectionInfo cnInfo;
 
   @Override
-  public List<ZipCode> findAll(Connection connection) throws Exception {
+  public void init(final ConnectionInfo cnInfo) {
+    this.cnInfo = cnInfo;
+  }
+
+  @Override
+  public List<ZipCode> findAll() throws Exception {
     final List<ZipCode> recordset = new ArrayList<>();
 
-    try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM t_zip_code;");
-        ResultSet resultset = statement.executeQuery();) {
+    try (Connection connection = this.getConnection(this.cnInfo);
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM t_zip_code;");
+        ResultSet resultset = statement.executeQuery(); ) {
       while (resultset.next()) {
         final ZipCode zipcode = new ZipCode();
 
@@ -41,69 +50,75 @@ public class ZipCodeRepositoryImpl implements ZipCodeRepository {
   }
 
   @Override
-  public boolean create(Connection connection, List<ZipCode> contents) throws Exception {
+  public boolean create(final List<ZipCode> contents) throws Exception {
     boolean status = false;
 
-    try (PreparedStatement statement = connection.prepareStatement(
-        "INSERT INTO t_zip_code (jis_code,zip_code,prefecture_phonetic,city_phonetic,area_phonetic,prefecture,city,area,update_flag,reason_flag) VALUES (?,?,?,?,?,?,?,?,?,?)")) {
-      connection.setAutoCommit(false);
-      long count = 0;
+    try (Connection connection = this.getConnection(this.cnInfo); ) {
+      try (PreparedStatement statement =
+          connection.prepareStatement(
+              "INSERT INTO t_zip_code (jis_code,zip_code,prefecture_phonetic,city_phonetic,area_phonetic,prefecture,city,area,update_flag,reason_flag) VALUES (?,?,?,?,?,?,?,?,?,?)")) {
+        connection.setAutoCommit(false);
+        long count = 0;
 
-      for (final ZipCode zipcode : contents) {
-        statement.setString(1, zipcode.getJisCode());
-        statement.setString(2, zipcode.getZipCode());
+        for (final ZipCode zipcode : contents) {
+          statement.setString(1, zipcode.getJisCode());
+          statement.setString(2, zipcode.getZipCode());
 
-        statement.setString(3, zipcode.getPrefecturePhonetic());
-        statement.setString(4, zipcode.getCityPhonetic());
-        statement.setString(5, zipcode.getAreaPhonetic());
+          statement.setString(3, zipcode.getPrefecturePhonetic());
+          statement.setString(4, zipcode.getCityPhonetic());
+          statement.setString(5, zipcode.getAreaPhonetic());
 
-        statement.setString(6, zipcode.getPrefecture());
-        statement.setString(7, zipcode.getCity());
-        statement.setString(8, zipcode.getArea());
+          statement.setString(6, zipcode.getPrefecture());
+          statement.setString(7, zipcode.getCity());
+          statement.setString(8, zipcode.getArea());
 
-        statement.setInt(9, zipcode.getUpdateFlag());
-        statement.setInt(10, zipcode.getReasonFlag());
+          statement.setInt(9, zipcode.getUpdateFlag());
+          statement.setInt(10, zipcode.getReasonFlag());
 
-        statement.addBatch();
-        count++;
+          statement.addBatch();
+          count++;
 
-        if (count % 1000 == 0) {
-          statement.executeBatch();
-          connection.commit();
-          connection.setAutoCommit(false);
+          if (count % 1000 == 0) {
+            statement.executeBatch();
+            connection.commit();
+            connection.setAutoCommit(false);
+          }
         }
-      }
 
-      statement.executeBatch();
-      connection.commit();
+        statement.executeBatch();
+        connection.commit();
 
-      if (count > 0) {
-        status = true;
+        if (count > 0) {
+          status = true;
+        }
+      } catch (final Exception e) {
+        connection.rollback();
+        throw e;
       }
-    } catch (final Exception e) {
-      connection.rollback();
-      throw e;
     }
 
     return status;
   }
 
   @Override
-  public boolean deleteAll(Connection connection) throws Exception {
+  @SuppressWarnings("unused")
+  public boolean deleteAll() throws Exception {
     boolean status = false;
 
-    try (PreparedStatement statement = connection.prepareStatement("DELETE FROM t_zip_code;")) {
-      connection.setAutoCommit(false);
+    try (Connection connection = this.getConnection(this.cnInfo); ) {
+      try (PreparedStatement statement = connection.prepareStatement("DELETE FROM t_zip_code;")) {
+        connection.setAutoCommit(false);
 
-      statement.addBatch();
-      statement.addBatch("ALTER TABLE t_zip_code auto_increment=1;");
-      statement.executeBatch();
+        statement.addBatch();
+        statement.addBatch("ALTER TABLE t_zip_code auto_increment=1;");
+        statement.executeBatch();
 
-      connection.commit();
-      status = true;
-    } catch (final Exception e) {
-      connection.rollback();
-      throw e;
+        connection.commit();
+        status = true;
+      } catch (final Exception e) {
+        connection.rollback();
+        throw e;
+      }
     }
 
     return status;
