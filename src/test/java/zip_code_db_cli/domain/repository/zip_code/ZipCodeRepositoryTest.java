8540deee5994bdcebc76c3a4523573f3eabf117f
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-12-18 01:42:43 +0900

    new file: src/test/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepositoryTest.java

---
+++ src/test/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepositoryTest.java
@@ -0,0 +1,9 @@
+package zip_code_db_cli.domain.repository.zip_code;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({EmptyTable.class, NotEmptyTable.class})
+public class ZipCodeRepositoryTest {}
