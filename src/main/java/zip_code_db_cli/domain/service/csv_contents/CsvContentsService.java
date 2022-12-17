3ce8535b09e080cdb95165e1a238611d3058f08e
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-12-18 01:45:56 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/csv_contents/CsvContentsService.java
    new method: init
      CRUD 操作の実行前に ContentsModel を設定するように修正。

--- src/main/java/zip_code_db_cli/domain/service/csv_contents/CsvContentsService.java
+++ src/main/java/zip_code_db_cli/domain/service/csv_contents/CsvContentsService.java
@@ -1,12 +1,19 @@
 package zip_code_db_cli.domain.service.csv_contents;
 
 import java.util.List;
+import java_itamae.domain.model.contents.ContentsModel;
+import java_itamae.domain.service.common.BaseService;
 import zip_code_db_cli.domain.model.ZipCode;
 
-/**
- * CSV の読み書きを操作する。
- */
-public interface CsvContentsService {
+/** CSV の読み書きを操作する。 */
+public interface CsvContentsService extends BaseService {
+  /**
+   * 初期化処理を実行する。
+   *
+   * @param model 操作対象とするファイルの情報を収めた {@link ContentsModel} を指定する。
+   */
+  void init(ContentsModel model);
+
   /**
    * CSV を読み込んで {@link ZipCode} の {@link List} に変換して返す。
    *
@@ -18,38 +25,25 @@ public interface CsvContentsService {
   /**
    * CSV を上書きする。
    *
-   * @param content 書込み対象とする情報を納めた {@link ZipCode} を指定する。
+   * @param contents 書込み対象とする {@link ZipCode} を収めた {@link List} を指定する。
    * @return status
-   *         <ul>
-   *         <li>true: 書込みに成功したことを表す。</li>
-   *         <li>false: 書込みを実行しなかったことを表す。</li>
-   *         </ul>
-   * @throws Exception {@link java.lang.Exception}
+   *     <ul>
+   *       <li>0: 何も実行せずに正常終了したことを表す
+   *       <li>1: 異常終了したことを表す。
+   *       <li>2: 書込みを実行して正常終了したことを表す。
+   *     </ul>
    */
-  boolean updateContent(ZipCode content) throws Exception;
-
-  /**
-   * CSV を一行追記する。
-   *
-   * @param content 書込み対象とする情報を納めた {@link ZipCode} を指定する。
-   * @return status
-   *         <ul>
-   *         <li>true: 追記に成功したことを表す。</li>
-   *         <li>false: 追記を実行しなかったことを表す。</li>
-   *         </ul>
-   * @throws Exception {@link java.lang.Exception}
-   */
-  boolean appendContent(ZipCode content) throws Exception;
+  int updateContents(List<ZipCode> contents);
 
   /**
    * CSV を空にする。
    *
    * @return status
-   *         <ul>
-   *         <li>true: 削除に成功したことを表す。</li>
-   *         <li>false: 削除を実行しなかったことを表す。</li>
-   *         </ul>
-   * @throws Exception {@link java.lang.Exception}
+   *     <ul>
+   *       <li>0: 何も実行せずに正常終了したことを表す
+   *       <li>1: 異常終了したことを表す。
+   *       <li>2: 書込みを実行して正常終了したことを表す。
+   *     </ul>
    */
-  boolean deleteContents() throws Exception;
+  int deleteContents();
 }
