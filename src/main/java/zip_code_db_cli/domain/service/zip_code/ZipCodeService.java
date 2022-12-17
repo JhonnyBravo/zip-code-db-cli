e0336c9db87cc1f6bb82dcb6b19cda385be7e8d1
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-12-18 01:55:01 +0900

    modified: src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
    抽象クラスからインターフェイスに変更。
    init メソッドを追加し、 CRUD 実行前に ConnectionInfo
    を設定するように修正。

--- src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
+++ src/main/java/zip_code_db_cli/domain/service/zip_code/ZipCodeService.java
@@ -1,43 +1,49 @@
 package zip_code_db_cli.domain.service.zip_code;
 
 import java.util.List;
-import java_itamae_connection.domain.service.connection.ConnectionService;
+import java_itamae.domain.service.common.BaseService;
+import java_itamae_connection.domain.model.ConnectionInfo;
 import zip_code_db_cli.domain.model.ZipCode;
 
-/**
- * t_zip_code へのレコードの読み書きを操作する。
- */
-public abstract class ZipCodeService extends ConnectionService {
+/** t_zip_code へのレコードの読み書きを操作する。 */
+public interface ZipCodeService extends BaseService {
+  /**
+   * 初期化処理を実行する。
+   *
+   * @param cnInfo DB の接続情報を収めた {@link ConnectionInfo} を指定する。
+   */
+  void init(ConnectionInfo cnInfo);
+
   /**
    * テーブルに登録されている全レコードを取得する。
    *
    * @return {@link ZipCode} の {@link List} を返す。
-   * @throws Exception {@link java.lang.Exception}
+   * @throws Exception {@link Exception}
    */
-  public abstract List<ZipCode> findAll() throws Exception;
+  List<ZipCode> findAll() throws Exception;
 
   /**
    * テーブルへレコードを一括登録する。
    *
    * @param recordset 登録対象とする {@link ZipCode} の {@link List} を指定する。
    * @return status
-   *         <ul>
-   *         <li>true: 登録に成功したことを表す。</li>
-   *         <li>false: 登録に失敗したことを表す。</li>
-   *         </ul>
-   * @throws Exception {@link java.lang.Exception}
+   *     <ul>
+   *       <li>0: 何も実行せずに正常終了したことを表す。
+   *       <li>1: 異常終了したことを表す。
+   *       <li>2: レコードを登録して正常終了したことを表す。
+   *     </ul>
    */
-  public abstract boolean create(List<ZipCode> recordset) throws Exception;
+  int create(List<ZipCode> recordset);
 
   /**
    * テーブルからレコードを全件削除する。
    *
    * @return status
-   *         <ul>
-   *         <li>true: 削除に成功したことを表す。</li>
-   *         <li>false: 削除を実行しなかったことを表す。</li>
-   *         </ul>
-   * @throws Exception {@link java.lang.Exception}
+   *     <ul>
+   *       <li>0: 何も実行せずに正常終了したことを表す。
+   *       <li>1: 異常終了したことを表す。
+   *       <li>2: レコードを削除して正常終了したことを表す。
+   *     </ul>
    */
-  public abstract boolean deleteAll() throws Exception;
+  int deleteAll();
 }
