1441eb168131fd4c7af5a608e736f3b3e1ca92a3
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-12-18 01:35:49 +0900

    modified: src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
    new method: init
      CRUD 操作の実行前に Connection を渡して保持するように修正。

--- src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
+++ src/main/java/zip_code_db_cli/domain/repository/zip_code/ZipCodeRepository.java
@@ -1,46 +1,51 @@
 package zip_code_db_cli.domain.repository.zip_code;
 
-import java.sql.Connection;
 import java.util.List;
+import java_itamae_connection.domain.model.ConnectionInfo;
+import java_itamae_connection.domain.repository.common.BaseRepository;
 import zip_code_db_cli.domain.model.ZipCode;
 
-/**
- * t_zip_code へのレコードの読み書きを操作する。
- */
-public interface ZipCodeRepository {
+/** t_zip_code へのレコードの読み書きを操作する。 */
+public interface ZipCodeRepository extends BaseRepository {
+  /**
+   * 初期化処理を実行する。
+   *
+   * @param cnInfo DB の接続情報を収めた {@link ConnectionInfo} を指定する。
+   */
+  void init(ConnectionInfo cnInfo);
+
   /**
    * テーブルに登録されている全レコードを取得する。
    *
-   * @param connection DB 接続に使用する {@link Connection} を指定する。
    * @return recordset {@link ZipCode} の {@link List} を返す。
    * @throws Exception {@link java.lang.Exception}
    */
-  List<ZipCode> findAll(Connection connection) throws Exception;
+  List<ZipCode> findAll() throws Exception;
 
   /**
    * テーブルへレコードを一括登録する。
    *
-   * @param connection DB 接続に使用する {@link Connection} を指定する。
    * @param contents 登録対象とする {@link ZipCode} の {@link List} を指定する。
    * @return status
-   *         <ul>
-   *         <li>true: レコードの登録に成功したことを表す。</li>
-   *         <li>false: レコードの登録が失敗したことを表す。</li>
-   *         </ul>
+   *     <ul>
+   *       <li>true: レコードの登録に成功したことを表す。
+   *       <li>false: レコードの登録が失敗したことを表す。
+   *     </ul>
+   *
    * @throws Exception {@link java.lang.Exception}
    */
-  boolean create(Connection connection, List<ZipCode> contents) throws Exception;
+  boolean create(List<ZipCode> contents) throws Exception;
 
   /**
    * テーブルからレコードを全件削除する。
    *
-   * @param connection DB 接続に使用する {@link Connection} を指定する。
    * @return status
-   *         <ul>
-   *         <li>true: レコードの削除に成功したことを表す。</li>
-   *         <li>false: レコードの削除が失敗したことを表す。</li>
-   *         </ul>
+   *     <ul>
+   *       <li>true: レコードの削除に成功したことを表す。
+   *       <li>false: レコードの削除が失敗したことを表す。
+   *     </ul>
+   *
    * @throws Exception {@link java.lang.Exception}
    */
-  boolean deleteAll(Connection connection) throws Exception;
+  boolean deleteAll() throws Exception;
 }
