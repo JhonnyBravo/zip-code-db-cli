d9c3e10ea11e59f114125cf0896e08d917d90742
Jhonny Bravo <sanfranceshika5@gmail.com>
2022-12-18 01:29:19 +0900

    renamed: src/test/java/zip_code_db_cli/domain/{repository/csv_contents/CsvContentsRepositoryTest.java -> component/csv_contents/CsvContentsComponentTest.java}
    Suite を導入。以下のクラスにテストを分割。
    
    * EmptyFile
    * NotEmptyFile

---
+++ src/test/java/zip_code_db_cli/domain/component/csv_contents/CsvContentsComponentTest.java
@@ -0,0 +1,9 @@
+package zip_code_db_cli.domain.component.csv_contents;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({EmptyFile.class, NotEmptyFile.class})
+public class CsvContentsComponentTest {}
